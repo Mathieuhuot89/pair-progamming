67ce1d248b5f82413fba1b1e0940f4f66ab0f2f1
Mathieu <huot.mathieuu@gmail.com>
2023-03-25 17:10:45 +0100

    affichage des categories correspond au fiche produit voiture

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,29 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Voiture;
+use App\Repository\MarqueRepository;
 use App\Repository\VoitureRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(VoitureRepository $voitureRepository): Response
-    {   
+    {
         $voitures = $voitureRepository->findAll();
         return $this->render('home/index.html.twig', [
-        'voitures' => $voitures,
+            'voitures' => $voitures,
         ]);
     }
 
-    #[Route('/voiture/{id}', name: 'app_article')]
-    public function article(VoitureRepository $voitureRepository, $id): Response
+    #[Route('/voiture/{id}', name: 'app_voiture')]
+    public function voiture(VoitureRepository $voitureRepository, $id): Response
     {
         $voiture = $voitureRepository->find($id);
 
         return $this->render('home/voiture.html.twig', [
             'voiture' => $voiture,
+        ]);
+    }
+
+    #[Route('/marque/{id}', name: 'app_marque')]
+    public function marque(EntityManagerInterface $entityManager, MarqueRepository $marqueRepository, VoitureRepository $voitureRepository, $id): Response
+    {
+        $voitures = $entityManager->getRepository(Voiture::class)->findBy(['marque' => $id]);
+        $marque = $marqueRepository->find($id);
+        // $voitures  = $voitureRepository->findBy(['marque' => $id]);
+
+        return $this->render('home/categorie.html.twig', [
+            'voitures' => $voitures,
+            'marque' => $marque,
         ]);
     }
 }
