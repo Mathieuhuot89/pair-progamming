6ced4fa9135ef7714e17f4546ecc0218ba553020
Mathieu <huot.mathieuu@gmail.com>
2023-03-27 11:50:56 +0200

    mis a jour template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -56,7 +56,6 @@ class HomeController extends AbstractController
         dump($id);
         $voiture = $entityManager->getRepository(Voiture::class)->findOneBy(['id' => $id]);
         $customer = $this->getUser();
-        // $jourDepart = $request->get
         
         // dump($request);die();
         // $marque = $marqueRepository->find($id);
@@ -64,7 +63,7 @@ class HomeController extends AbstractController
         $form = $this->createForm(CommandeType::class, $commande);
         $form->handleRequest($request);
         $commande->setCreatedAt(new \DateTimeImmutable());
-        // $commande
+        
 
         if ($form->isSubmitted() && $form->isValid()) {
             $diff = date_diff($commande->getJourDepart(), $commande->getJourArrive());
@@ -82,6 +81,8 @@ class HomeController extends AbstractController
             $entityManager->persist($voiture);
             $entityManager->flush();
 
+            // add flashes - app.flashes
+
             return $this->render('home/reservation.html.twig', [
                 'reservationForm' => $form->createView(),
                 'stock' => $voiture->getStock(),
@@ -97,15 +98,4 @@ class HomeController extends AbstractController
         ]);
     }
 
-
-
-    // public function calculPrix(\DateTime $startDate, \DateTime $endDate)
-    // {
-    //     $pricePerDay = 10; // Prix par jour de location
-    //     $nbDays = $endDate->diff($startDate)->days + 1; // Nombre de jours de location
-    //     $totalPrice = $nbDays * $pricePerDay; // Calcul du prix total
-
-    //     // Retourne une réponse contenant le prix calculé au format JSON
-    //     return new Response(json_encode(['price' => $totalPrice]));
-    // }
 }
